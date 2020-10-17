a0196e24d7c123865dfcabaf20c2a9cf93f2bd44
jeodaysju <jeodaysju@yahoo.com>
2020-10-16 22:10:38 -0500

    Made the markers circles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,5 @@
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-10-08&endtime=2020-10-15&minmagnitude=2";
 
-
-
 var myMap = L.map("mapid", {
   center: [
     37.09, -95.71
@@ -19,24 +17,20 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
 
 d3.json(queryUrl).then(function(data) {
-  L.geoJson(data).addTo(myMap);
+  L.geoJSON(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  }).addTo(myMap);
 });
 
-// d3.json(queryUrl, function(data) {
-//   // Once we get a response, send the data.features object to the createFeatures function
-//   createFeatures(data.features);
-// });
-
-// function createFeatures(earthquakeData) {
-
-//   // Create a GeoJSON layer containing the features array on the earthquakeData object
-//   // Run the onEachFeature function once for each piece of data in the array
-//   var earthquakes = L.geoJSON(earthquakeData, {
-//     onEachFeature: onEachFeature
-//   });
-
-//   // Sending our earthquakes layer to the createMap function
-//   createMap(earthquakes);
-// }
