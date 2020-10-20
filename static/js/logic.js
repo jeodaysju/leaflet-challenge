38ce6179c89ea8b0747b9c71b0a080b666af7a18
jeodaysju <jeodaysju@yahoo.com>
2020-10-19 21:02:25 -0500

    have partial legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ var myMap = L.map("mapid", {
   center: [
     37.09, -95.71
   ],
-  zoom: 5,
+  zoom: 3,
 });
 
 function markerSize(magnitude) {
@@ -16,14 +16,13 @@ function opacityShade(depth) {
 }
 
 // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
 
 function chooseColor(depth) {
 
@@ -52,7 +51,6 @@ function scaledRadius(latitude, initialRadius)
   return radiusOfEarth * Math.acos(Math.cos(initialRadius / radiusOfEarth) * cosLat * cosLat + sinLat * sinLat);
 }
 
-
 d3.json(queryUrl).then(function (data) {
 
   L.geoJSON(data,
@@ -70,4 +68,36 @@ d3.json(queryUrl).then(function (data) {
       }
     }).addTo(myMap);
 });
+
+
+function getColor(d) {
+  return d === 'Depth -10 to 10'  ? "lightyellow" :
+         d === 'Depth 10 to 30'  ? "yellow" :
+         d === 'Depth 30 to 50' ? "lightgreen" :
+         d === 'Depth 50 to 70' ? "green" :
+         d === 'Depth 70 to 90' ? "darkorange" :
+         d === 'Depth Greater Than 90' ? "red" :
+                      "black";
+}
+
+var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    labels = ['<strong>Legend</strong>'],
+    categories = ['Depth -10 to 10','Depth 10 to 30','Depth 30 to 50','Depth 50 to 70','Depth 70 to 90','Depth Greater Than 90'];
+
+    for (var i = 0; i < categories.length; i++) {
+
+            div.innerHTML += 
+            labels.push(
+                '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +
+            (categories[i] ? categories[i] : '+'));
+
+        }
+        div.innerHTML = labels.join('<br>');
+    return div;
+    };
+    legend.addTo(myMap);
+
 
